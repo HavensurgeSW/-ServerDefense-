6ac685b95b15e9f4297744987952e5eb1a4dc1cb
Matias Karplus <50681644+HavensurgeSW@users.noreply.github.com>
2022-10-26 22:06:19 -0300

    Added Location spotlights
    Trigger on selection with CD command

--- TowerSecurity/Assets/Game/Gameplay/Modules/MapHandler/Scripts/Location.cs
+++ TowerSecurity/Assets/Game/Gameplay/Modules/MapHandler/Scripts/Location.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Location : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer sr;
     [SerializeField] private string id;
     [SerializeField] private bool availability;
+    [SerializeField] private Light2D selectedSpotlight;
 
     private BaseTower tower = null;
     private bool isSelected = false;
@@ -28,6 +30,8 @@ public class Location : MonoBehaviour
     public void ToggleSelected(bool b)
     {
         isSelected = b;
+        selectedSpotlight.enabled = b;
+        
     }
 
     public void ToggleColor(Color clr)
