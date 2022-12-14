99a3d8bcd87bda1fad36e20f5b431e59b91508a1
Matias Karplus <50681644+HavensurgeSW@users.noreply.github.com>
2022-12-14 20:57:55 -0300

    Created PlayerPrefs utils for audio settings

--- ServerDefense/Assets/GameSFXUtils.cs
+++ ServerDefense/Assets/GameSFXUtils.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSFXUtils : MonoBehaviour
 {
-    LevelManager yeet;
+    [Header("Sliders")]
+    [SerializeField] Slider axSlider;
+    [SerializeField] Slider mxSlider;
+    [SerializeField] Slider tmlSlider;
+    [SerializeField] Slider wldSlider;
 
+    [Header("Bus names")]
+    [SerializeField]string busAX = null;
+    [SerializeField]string busMX = null;
+    [SerializeField]string busTerminal = null;
+    [SerializeField]string busWorld = null;
 
-    private void Awake()
-    {
-        //AkSoundEngine.SetState();
-       
+    public void SetAudioLevels() {
+        AkSoundEngine.SetRTPCValue(busAX, axSlider.value);
+        AkSoundEngine.SetRTPCValue(busMX, mxSlider.value);
+        AkSoundEngine.SetRTPCValue(busTerminal, tmlSlider.value);
+        AkSoundEngine.SetRTPCValue(busWorld, wldSlider.value);
+
+        PlayerPrefs.SetFloat(busAX, axSlider.value);
+        PlayerPrefs.SetFloat(busMX, axSlider.value);
+        PlayerPrefs.SetFloat(busTerminal, axSlider.value);
+        PlayerPrefs.SetFloat(busWorld, axSlider.value);
+    
+    }
+
+    public void LoadAudioLevelsFromPrefs() {
+        AkSoundEngine.SetRTPCValue(busAX, PlayerPrefs.GetFloat(busAX));
+        AkSoundEngine.SetRTPCValue(busMX, PlayerPrefs.GetFloat(busMX));
+        AkSoundEngine.SetRTPCValue(busTerminal, PlayerPrefs.GetFloat(busTerminal));
+        AkSoundEngine.SetRTPCValue(busWorld, PlayerPrefs.GetFloat(busWorld));
     }
 }
