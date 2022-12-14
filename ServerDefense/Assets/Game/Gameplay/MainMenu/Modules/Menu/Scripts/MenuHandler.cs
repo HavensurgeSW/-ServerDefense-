6f9fad53b6dfebdcf31bfc8d38b02989d5874884
Matias Karplus <50681644+HavensurgeSW@users.noreply.github.com>
2022-12-14 19:53:24 -0300

    adjusted level lights, updated splash video HD

--- ServerDefense/Assets/Game/Gameplay/MainMenu/Modules/Menu/Scripts/MenuHandler.cs
+++ ServerDefense/Assets/Game/Gameplay/MainMenu/Modules/Menu/Scripts/MenuHandler.cs
@@ -22,7 +22,9 @@ public class MenuHandler : MonoBehaviour
     [SerializeField] private Button level1Button = null;
     [SerializeField] private Button backButton = null;
 
-    [Header("Audio Config")]
+    [Header("Audio setup")]
+    [SerializeField] private GameObject optionsHolder = null;
+    [SerializeField] private Button optionsBackButton = null;
     [SerializeField] private GameObject sfx_source = null;
 
     public void Init(Action onSwitchToCredits, Action<SCENE, bool> onChangeScene, Action onExitGame)
@@ -37,6 +39,18 @@ public class MenuHandler : MonoBehaviour
             ToggleLevelStatus(false);
             backButton.onClick.AddListener(() => { AkSoundEngine.PostEvent("Play_UI_Button", gameObject); });
         });
+
+        optionsButton.onClick.AddListener(() => {
+            ToggleOptionsStatus(true);
+            ToggleMenuStatus(false);
+        });
+
+        optionsBackButton.onClick.AddListener(() => {
+            ToggleMenuStatus(true);
+            ToggleOptionsStatus(false);
+            optionsBackButton.onClick.AddListener(() => { AkSoundEngine.PostEvent("Play_UI_Button", gameObject); });
+        });
+
         level0Button.onClick.AddListener(() => onChangeScene?.Invoke(SCENE.LEVEL_0, true));
         level0Button.onClick.AddListener(() => { AkSoundEngine.PostEvent("Play_UI_Button", gameObject); });
         level1Button.onClick.AddListener(() => onChangeScene?.Invoke(SCENE.LEVEL_1, true));
@@ -58,7 +72,10 @@ public class MenuHandler : MonoBehaviour
 
     public void ToggleLevelStatus(bool status)
     {
-        levelHolder.SetActive(status);
-        
+        levelHolder.SetActive(status);   
+    }
+
+    public void ToggleOptionsStatus(bool status) { 
+        optionsHolder.SetActive(status);
     }
 }
