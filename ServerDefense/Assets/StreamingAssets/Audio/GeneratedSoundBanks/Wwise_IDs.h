c99ccb6438a096e78aa0bf8cb6b296332f243424
MariangelesB <mariangelesbgs@gmail.com>
2022-12-14 21:50:02 -0300

    Implementación Wwise Completa

--- ServerDefense/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ ServerDefense/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,62 +11,24 @@
 
 namespace AK
 {
-    namespace EVENTS
+    namespace STATES
     {
-        static const AkUniqueID PLAY_ANTIVIRUS_1_BLAST = 22560834U;
-        static const AkUniqueID PLAY_ANTIVIRUS_1_LASER = 2506060853U;
-        static const AkUniqueID PLAY_ANTIVIRUS_2_BLAST = 1410437897U;
-        static const AkUniqueID PLAY_ANTIVIRUS_2_LASER = 2192258458U;
-        static const AkUniqueID PLAY_ANTIVIRUS_3_BLAST = 2643594368U;
-        static const AkUniqueID PLAY_ANTIVIRUS_3_LASER = 473703163U;
-        static const AkUniqueID PLAY_AX_GAMESCENE = 3618950782U;
-        static const AkUniqueID PLAY_AX_MENU = 327270693U;
-        static const AkUniqueID PLAY_AX_TUTORIAL = 636914782U;
-        static const AkUniqueID PLAY_CD_LOC = 2320647108U;
-        static const AkUniqueID PLAY_FIREWALL_1_BLAST = 3541071973U;
-        static const AkUniqueID PLAY_FIREWALL_1_LASER = 4004066118U;
-        static const AkUniqueID PLAY_FIREWALL_2_BLAST = 4045841438U;
-        static const AkUniqueID PLAY_FIREWALL_2_LASER = 2005936881U;
-        static const AkUniqueID PLAY_FIREWALL_3_BLAST = 3225411671U;
-        static const AkUniqueID PLAY_FIREWALL_3_LASER = 17370536U;
-        static const AkUniqueID PLAY_GAME_OVER = 725000905U;
-        static const AkUniqueID PLAY_HELP = 2454594115U;
-        static const AkUniqueID PLAY_INSTALL_ANTI = 2006190918U;
-        static const AkUniqueID PLAY_INSTALL_FIRE = 2336984686U;
-        static const AkUniqueID PLAY_NETWORK_INIT = 2665061269U;
-        static const AkUniqueID PLAY_SD_LOGO = 3651786367U;
-        static const AkUniqueID PLAY_SD_TITLE = 3888294756U;
-        static const AkUniqueID PLAY_SFX_WIN = 1910125964U;
-        static const AkUniqueID PLAY_SWARM_DIES = 1484225356U;
-        static const AkUniqueID PLAY_SWARM_PASS = 1662830590U;
-        static const AkUniqueID PLAY_UI_BUTTON = 43599987U;
-        static const AkUniqueID PLAY_UI_HOVER = 1339559671U;
-        static const AkUniqueID PLAY_UNINSTALL_ANTI = 250668327U;
-        static const AkUniqueID PLAY_UPDATE_DEPLOY_ANTI = 3792050540U;
-        static const AkUniqueID PLAY_UPDATE_DEPLOY_FIRE = 2932367912U;
-        static const AkUniqueID PLAY_UPDATE_INFO = 3813994196U;
-        static const AkUniqueID PLAY_VIRUS_DIES = 1968882817U;
-        static const AkUniqueID PLAY_VIRUS_PASS = 427127591U;
-        static const AkUniqueID PLAY_VX_SD_SKULL = 3595651944U;
-        static const AkUniqueID PLAY_WAVE_BAR = 3055448759U;
-        static const AkUniqueID PLAY_WRONG = 92581801U;
-        static const AkUniqueID STOP_ANTIVIRUS_1_BLAST = 2424970460U;
-        static const AkUniqueID STOP_ANTIVIRUS_1_LASER = 2516319623U;
-        static const AkUniqueID STOP_ANTIVIRUS_2_BLAST = 882063095U;
-        static const AkUniqueID STOP_ANTIVIRUS_2_LASER = 2010462792U;
-        static const AkUniqueID STOP_ANTIVIRUS_3_BLAST = 4211330366U;
-        static const AkUniqueID STOP_ANTIVIRUS_3_LASER = 2129848849U;
-        static const AkUniqueID STOP_AX_GAMESCENE = 3726213320U;
-        static const AkUniqueID STOP_AX_MENU = 3166435559U;
-        static const AkUniqueID STOP_AX_TUTORIAL = 3824642668U;
-        static const AkUniqueID STOP_FIREWALL_1_BLAST = 3462881543U;
-        static const AkUniqueID STOP_FIREWALL_1_LASER = 4018147032U;
-        static const AkUniqueID STOP_FIREWALL_2_BLAST = 3709894380U;
-        static const AkUniqueID STOP_FIREWALL_2_LASER = 2834782679U;
-        static const AkUniqueID STOP_FIREWALL_3_BLAST = 1169078421U;
-        static const AkUniqueID STOP_FIREWALL_3_LASER = 1131542710U;
-        static const AkUniqueID STOP_VX_SD_SKULL = 2636698450U;
-    } // namespace EVENTS
+        namespace LEVEL
+        {
+            static const AkUniqueID GROUP = 2782712965U;
+
+            namespace STATE
+            {
+                static const AkUniqueID EASY = 529018163U;
+                static const AkUniqueID HARD = 3599861390U;
+                static const AkUniqueID LOOSE = 174960569U;
+                static const AkUniqueID MEDIUM = 2849147824U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID WIN = 979765101U;
+            } // namespace STATE
+        } // namespace LEVEL
+
+    } // namespace STATES
 
     namespace GAME_PARAMETERS
     {
